7980fbf01b683735a1850c080b237012b0d4c602
le tuan dat <letuandattrumrules@gmail.com>
2023-10-27 22:33:25 +0700

    delete

--- Project04/Project04.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/mushrooms.csv")
-data_X = data.drop("class", axis=1)
-data_Y = data.iloc[:, 0]
-
