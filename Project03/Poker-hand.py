6981e1b03cfd2d3d1642200b43121ff45ebea6ae
le tuan dat <letuandattrumrules@gmail.com>
2023-11-06 21:42:03 +0700

    update poker

--- Project03/Poker-hand.py
+++ Project03/Poker-hand.py
@@ -1,116 +1,140 @@
-import pandas as pd
 import numpy as np
-import time
+import pandas as pd
 import matplotlib.pyplot as plt
 
+from sklearn.model_selection import KFold
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import *
-from sklearn.metrics import accuracy_score, confusion_matrix, precision_recall_fscore_support, f1_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score
 
-# SPLITTING DATA TO X & Y
-data_train = pd.read_csv("poker-hand-training-true.data", header=None)
-data_train_X = data_train.iloc[:, :-1]
-data_train_y = data_train.iloc[:, -1]
+data = pd.read_csv("poker-hand.csv", header=None)
+data_X = data.iloc[:, :-1]
+data_y = data.iloc[:, -1]
 
-data_test = pd.read_csv("poker-hand-testing.data", header=None)
-data_test_X = data_test.iloc[:, :-1]
-data_test_y = data_test.iloc[:, -1]
+kf = KFold(n_splits=10, shuffle=True)
 
-# READING DATA DESCRIPTION
-print(f"Number of samples in the training set: {len(data_train)}")
-print(f"Number of samples in the test set: {len(data_test)}")
-print(f"Labels in the dataset: {np.unique(data_test_y)}")
-print(f"Number of each label in the training set:\n{data_train_y.value_counts()}")
-print(f"Number of each label in the test set:\n{data_test_y.value_counts()}")
-# # Lấy tên thuộc tính
+fc_avg_knn = 0
+fc_avg_tree = 0
+fc_avg_rd = 0
+rd_features_importance = 0
+cnt = 0
 
-# KNN
-start_knn = time.time()
+for train_idx, test_idx in kf.split(data_X):
+    print(f"Lần lặp thứ {cnt + 1}:")
+    data_train_X, data_test_X = data_X.iloc[train_idx, ], data_X.iloc[test_idx, ]
+    data_train_y, data_test_y = data_y.iloc[train_idx], data_y.iloc[test_idx]
 
-Model_KNN = KNeighborsClassifier(n_neighbors=11)
-Model_KNN.fit(data_train_X, data_train_y)
+    # print(f"    Số lượng tập train: {len(data_train_X)}")
+    # print(f"    Số lượng tập test: {len(data_test_X)}")
 
-data_pred_train_KNN = Model_KNN.predict(data_train_X)
-data_pred_test_KNN = Model_KNN.predict(data_test_X)
+    # --------
 
-rate_train_KNN = accuracy_score(data_train_y, data_pred_train_KNN) * 100
-rate_test_KNN = accuracy_score(data_test_y, data_pred_test_KNN) * 100
+    Model_KNN = KNeighborsClassifier(n_neighbors=11)
+    Model_KNN.fit(data_train_X, data_train_y)
 
-rate_train_KNN_fc = f1_score(data_train_y, data_pred_train_KNN, average=None)
-rate_test_KNN_fc = f1_score(data_train_y, data_pred_train_KNN, average=None)
+    data_pred_knn = Model_KNN.predict(data_test_X)
 
-knn_results = pd.DataFrame(['KNeighborClassifier', round(rate_train_KNN, 2), round(rate_test_KNN, 2)]).transpose()
-knn_results.columns = ['Method', 'ACS Train', 'ACS Test']
+    rate_knn_fc = round(f1_score(data_test_y, data_pred_knn, average='micro') * 100, 2)
+    print(f"    Độ chính xác KNN: {rate_knn_fc}")
+    fc_avg_knn += rate_knn_fc
 
-end_knn = time.time()
+    # --------
 
-# DECISION TREE
-start_dt = time.time()
+    Model_Tree = DecisionTreeClassifier(criterion="entropy")
+    Model_Tree.fit(data_train_X, data_train_y)
 
-Model_Tree = DecisionTreeClassifier(criterion="entropy", random_state=100, max_depth=3)
-Model_Tree.fit(data_train_X, data_train_y)
+    data_pred_tree = Model_Tree.predict(data_test_X, check_input=True)
 
-data_pred_train_tree = Model_Tree.predict(data_train_X)
-data_pred_test_tree = Model_Tree.predict(data_test_X)
+    rate_tree_fc = round(f1_score(data_test_y, data_pred_tree, average='micro') * 100, 2)
+    print(f"    Độ chính xác DT: {rate_tree_fc}")
+    fc_avg_tree += rate_tree_fc
 
-rate_train_tree = accuracy_score(data_train_y, data_pred_train_tree) * 100
-rate_test_tree = accuracy_score(data_test_y, data_pred_test_tree) * 100
+    # --------
 
-rate_train_tree_fc = f1_score(data_train_y, data_pred_train_tree, average=None)
-rate_test_tree_fc = f1_score(data_test_y, data_pred_test_tree, average=None)
+    Model_RD = RandomForestClassifier(criterion="entropy")
+    Model_RD.fit(data_train_X, data_train_y)
 
-tree_results = pd.DataFrame(['Decision Tree', round(rate_train_tree, 2), round(rate_test_tree, 2)]).transpose()
-tree_results.columns = ['Method', 'ACS Train', 'ACS Test']
+    data_pred_rd = Model_RD.predict(data_test_X)
 
-end_dt = time.time()
+    rate_rd_fc = round(f1_score(data_test_y, data_pred_rd, average='micro') * 100, 2)
+    print(f"    Độ chính xác RD: {rate_rd_fc}\n")
+    fc_avg_rd += rate_rd_fc
 
-# RANDOM FOREST
-start_rd = time.time()
+    rd_features_importance += Model_RD.feature_importances_
 
-Model_RD = RandomForestClassifier(criterion="entropy", n_estimators=50, random_state=100)
-Model_RD.fit(data_train_X, data_train_y)
+    # --------
 
-data_pred_train_rd = Model_RD.predict(data_train_X)
-data_pred_test_rd = Model_RD.predict(data_test_X)
+    cnt += 1
 
-rate_train_rd = accuracy_score(data_train_y, data_pred_train_rd) * 100
-rate_test_rd = accuracy_score(data_test_y, data_pred_test_rd) * 100
+print(f"Độ chính xác trung bình f1 KNN: {fc_avg_knn // cnt}")
+print(f"Độ chính xác trung bình f1 DT: {fc_avg_tree // cnt}")
+print(f"Độ chính xác trung bình f1 RD: {fc_avg_rd // cnt}")
 
-rate_train_rd_fc = f1_score(data_train_y, data_pred_train_rd, average=None)
-rate_test_rd_fc = f1_score(data_test_y, data_pred_test_rd, average=None)
+# Biểu đồ dữ liệu
+value_counts_train = data_y.value_counts()
+label_name_train = []
+number_of_labels_train = []
+j = 0
 
-rd_results = pd.DataFrame(['Random Forest', round(rate_train_rd, 2), round(rate_test_rd, 2)]).transpose()
-rd_results.columns = ['Method', 'ACS Train', 'ACS Test']
+for value, count in value_counts_train.items():
+    label_name_train.append(j)
+    number_of_labels_train.append(count)
+    j += 1
 
-end_rd = time.time()
+plt.bar(label_name_train, number_of_labels_train)
+plt.title('Number of each type of train label')
+plt.xlabel('Label type')
+plt.ylabel('Label quantity')
 
-# MODEL COMPARISON (So sánh chỉ số đánh giá các mô hình)
-poker_results = pd.concat([knn_results, tree_results, rd_results], axis=0).reset_index(drop=True)
-print(poker_results)
+for i, value1 in enumerate(number_of_labels_train):
+    plt.text(i, value1, str(value1), ha='center', va='bottom')
+plt.show()
 
-# VISUALIZING (Vẽ biểu đồ)
-
-# 1. Biểu đồ chỉ số đánh giá 3 giải thuật
-categories = ['KNN','DT', 'RD']
-values = [round(rate_test_KNN, 2), round(rate_test_tree, 2), round(rate_test_rd, 2)]
+# Biều đồ độ chính xác
+categories = ['KNN', 'DT', 'RD']
+values = [fc_avg_knn // cnt, fc_avg_tree // cnt, fc_avg_rd // cnt]
 
 plt.bar(categories, values)
-
 plt.title('Accuracy score of 3 algorithms')
 
-plt.xlabel('algorithm name')
-plt.ylabel('precision value')
+plt.xlabel('Algorithm name')
+plt.ylabel('Precision value')
 
 for i, value in enumerate(values):
     plt.text(i, value, str(value), ha='center', va='bottom')
 
 plt.show()
 
-# 2. Biểu đồ tương quan dữ liệu
+# Biểu đồ thuộc tính quan trọng
+feature_names = ['Chất lá 1', 'Số lá 1', 'Chất lá 2', 'Số lá 2', 'Chất lá 3', 'Số lá 3', 'Chất lá 4', 'Số lá 4', 'Chất lá 5', 'Số lá 5']
+plt.figure(figsize=(10, 6))
+plt.barh(feature_names, rd_features_importance / cnt)
+plt.xlabel("Feature Importance")
+plt.ylabel("Features")
+plt.title("Random Forest Feature Importance")
+plt.show()
 
+# Cây => Luật
+# tree_rules = export_text(Model_Tree, feature_names=feature_names)
 
-# TIME TRAINING & TEST MODEL OF 3 ALGORITHMS
-print(f"\nTime for KNN: {end_knn - start_knn}")
-print(f"Time for DecisionTree: {end_dt - start_dt}")
-print(f"Time for Random Forest: {end_rd - start_rd}")
+def extract_rules(tree, feature_names):
+    rules = []
+    stack = [(0, -1)]  # Dùng một ngăn xếp để theo dõi node và depth
+    while stack:
+        node, depth = stack.pop()
+        if node < 0:
+            continue
+        if depth > 0:
+            if tree.tree_.children_left[node] == tree.tree_.children_right[node]:
+                value = np.argmax(tree.tree_.value[node])
+                rule = "THEN Class {} (leaf node)".format(value)
+            else:
+                rule = "IF {} <= {}".format(feature_names[tree.tree_.feature[node]], tree.tree_.threshold[node])
+            rules.append("{}{}".format("  " * depth, rule))
+        stack.append((tree.tree_.children_left[node], depth + 1))
+        stack.append((tree.tree_.children_right[node], depth + 1))
+    return rules
+
+tree_rules = extract_rules(Model_Tree, feature_names)
+print(tree_rules)
